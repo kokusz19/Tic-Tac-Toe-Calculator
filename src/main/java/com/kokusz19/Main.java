bcaf9b932f79e6c7cbc5c16ffe0af524b7a0a539
kokusz19 <kokkusz1996@hotmail.com>
2024-04-13 11:43:47 +0200

    Initial commit

---
+++ src/main/java/com/kokusz19/Main.java
@@ -0,0 +1,12 @@
+package com.kokusz19;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class Main {
+
+   public static void main(String[] args) {
+       System.out.println("Hello world!");
+    }
+    
+}
